3b5c988bff158df2772bdb3b678e82249c4113f1
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-19 09:14:30 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springchadmvcvalidation/SpringChadMvcValidationApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springchadmvcvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChadMvcValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChadMvcValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iffat/springchadmvcvalidation/SpringChadMvcValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.springchadmvcvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChadMvcValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

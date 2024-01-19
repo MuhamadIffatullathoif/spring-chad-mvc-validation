63d42cfd819644977878266d3d927ca4ad9f0076
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-19 10:44:55 +0700

    03. Spring MVC Validation - Validate a number range

--- src/main/java/com/iffat/springchadmvcvalidation/Customer.java
+++ src/main/java/com/iffat/springchadmvcvalidation/Customer.java
@@ -1,5 +1,7 @@
 package com.iffat.springchadmvcvalidation;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "must greater than or equal zero")
+    @Max(value = 10, message = "must less than or equal ten")
+    private int freePasses;
 
     public Customer() {
     }
@@ -27,5 +33,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }

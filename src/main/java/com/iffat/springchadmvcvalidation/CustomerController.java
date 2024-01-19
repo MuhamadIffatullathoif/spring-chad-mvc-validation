55860b4b68d42a482a12d337f76883a2d146046f
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-19 10:03:43 +0700

    02. Spring MVC Validation - @InitBinder

--- src/main/java/com/iffat/springchadmvcvalidation/CustomerController.java
+++ src/main/java/com/iffat/springchadmvcvalidation/CustomerController.java
@@ -1,15 +1,24 @@
 package com.iffat.springchadmvcvalidation;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showCustomerForm(Model model) {
@@ -19,7 +28,7 @@ public class CustomerController {
 
     @PostMapping("/processCustomerForm")
     public String processCustomerForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             return "customer-form";
         } else {
             return "customer-confirmation";
